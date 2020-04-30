84a04fd6db3127418223d9f0dbdf1253b6e40db2
Danielle Korn <daniella.korn@gmail.com>
2020-04-30 19:22:15 +0300

    skelton node server

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+	res.send("Hello");
+});
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));

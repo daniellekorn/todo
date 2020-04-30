a35d245ae9d826e20ceff6709f5bd51a6eed7c13
Danielle Korn <daniella.korn@gmail.com>
2020-04-30 16:35:31 +0300

    removed cross-out for done items

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,16 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class TodoItem extends React.Component {
-	getStyle = () => {
-		return {
-			textDecoration: this.props.todo.completed ? "line-through" : "none",
-		};
-	};
-
 	render() {
 		const { id, title } = this.props.todo;
 		return (
-			<div style={this.getStyle()}>
+			<div>
 				<p>
 					<input
 						type="checkbox"
